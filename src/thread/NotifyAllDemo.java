4c56b7e4510b742819b70b9a9e38140b818268a3
dapengyou <ladyztt@sina.com>
2019-03-05 11:34:14 +0800

    线程唤醒demo使用同步锁锁住同一个对象

--- src/thread/NotifyAllDemo.java
+++ src/thread/NotifyAllDemo.java
@@ -1,35 +1,42 @@
 package thread;
 
 public class NotifyAllDemo {
-    private static Object obj = new Object();
-    public static void main(String[] args) {
+//    private static Object obj = new Object();
 
-        ThreadA t1 = new ThreadA("t1");
-        ThreadA t2 = new ThreadA("t2");
-        ThreadA t3 = new ThreadA("t3");
-        t1.start();
-        t2.start();
-        t3.start();
+    public static void main(String[] args) {
+        Object obj = new Object();
+
+        for (int i = 0; i < 3; i++) {
+            ThreadA t1 = new ThreadA(obj, "t" + i);
+            t1.start();
+        }
+//        ThreadA t2 = new ThreadA("t2");
+//        ThreadA t3 = new ThreadA("t3");
+//        t1.start();
+//        t2.start();
+//        t3.start();
 
         try {
-            System.out.println(Thread.currentThread().getName()+" sleep(3000)");
+            System.out.println(Thread.currentThread().getName() + " sleep(3000)");
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        synchronized(obj) {
+        synchronized (obj) {
             // 主线程等待唤醒。
-            System.out.println(Thread.currentThread().getName()+" notifyAll()");
+            System.out.println(Thread.currentThread().getName() + " notifyAll()");
             obj.notifyAll();
         }
 
     }
 
-    static class ThreadA extends Thread{
+    static class ThreadA extends Thread {
+        private Object obj;//使用同一个对象
 
-        public ThreadA(String name){
+        public ThreadA(Object obj, String name) {
             super(name);
+            this.obj = obj;
         }
 
         public void run() {
